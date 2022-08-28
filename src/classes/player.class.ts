74d9659da6983dcb4335138f6197e74acd28a68c
Alexander Hridin <alexander.m.gridin@gmail.com>
2022-08-28 14:28:48 +0300

    player movement and collision detection updated

--- src/classes/player.class.ts
+++ src/classes/player.class.ts
@@ -1,12 +1,20 @@
 import { InputKey } from "../enumerations/input-key.enum";
 import { colors } from "../static-data/colors";
+import { Canvas } from "./canvas.class";
 import { GameObject } from "./game-object.class";
 import { SharedGameData } from "./shared-game-data.class";
 
 interface CollisionData {
   xCollision: boolean;
   yCollision: boolean;
+
+  topCollision: boolean;
+  rightCollision: boolean;
+  bottomCollision: boolean;
+  leftCollision: boolean;
 }
+
+type Direction = "top" | "right" | "bottom" | "left" | "none";
 
 export class Player extends GameObject {
   public readonly width = 64;
@@ -17,14 +25,7 @@ export class Player extends GameObject {
 
   public posX!: number;
   public posY!: number;
-
-  public directionX = 1;
-  public directionY = 1;
-
-  private initialAngle = 0;
-  private angle = this.initialAngle;
-  private rotationSpeed = 3;
-  private rotationDirection = 1;
+  public direction!: Direction;
 
   private collisionData!: CollisionData;
   private gameData!: SharedGameData;
@@ -43,22 +44,9 @@ export class Player extends GameObject {
     this.gameData = gameData;
 
     this.handleHelpers();
-    this.calculateMovement();
     this.handleWorldCollision();
-  }
-
-  private getCollisionData(gameGeometry: {
-    width: number;
-    height: number;
-  }): CollisionData {
-    return {
-      xCollision:
-        this.posX - this.speed < this.width * 0.5 ||
-        this.posX + this.speed + this.width * 0.5 > gameGeometry.width,
-      yCollision:
-        this.posY - this.speed < this.height * 0.5 ||
-        this.posY + this.speed + this.height * 0.5 > gameGeometry.height,
-    };
+    this.detectDirection();
+    this.calculateMovement();
   }
 
   private handleHelpers(): void {
@@ -73,100 +61,135 @@ export class Player extends GameObject {
     }
   }
 
+  private detectDirection(): void {
+    const { inputHandler } = this.gameData;
+
+    if (inputHandler.isKeyClicked(InputKey.W)) {
+      this.direction = "top";
+      return;
+    }
+
+    if (inputHandler.isKeyClicked(InputKey.S)) {
+      this.direction = "bottom";
+      return;
+    }
+
+    if (inputHandler.isKeyClicked(InputKey.D)) {
+      this.direction = "right";
+      return;
+    }
+
+    if (inputHandler.isKeyClicked(InputKey.A)) {
+      this.direction = "left";
+      return;
+    }
+  }
+
   private calculateMovement(): void {
     const { inputHandler } = this.gameData;
-    const { xCollision, yCollision } = this.collisionData;
 
-    if (inputHandler.isKeyPressed(InputKey.K)) {
-      if (this.angle >= 360 || this.angle <= -360) {
-        this.angle = 0 + this.rotationSpeed * this.rotationDirection;
-      } else {
-        this.angle += this.rotationSpeed * this.rotationDirection;
-      }
+    if (inputHandler.isKeyPressed(InputKey.W) && this.direction === "top") {
+      this.moveForward();
+      return;
     }
 
-    if (inputHandler.isKeyPressed(InputKey.J)) {
-      if (this.angle >= 360 || this.angle <= -360) {
-        this.angle = 0 + this.rotationSpeed * -this.rotationDirection;
-      } else {
-        this.angle += this.rotationSpeed * -this.rotationDirection;
-      }
+    if (inputHandler.isKeyPressed(InputKey.S) && this.direction === "bottom") {
+      this.moveBakward();
+      return;
     }
 
-    if (inputHandler.isKeyPressed(InputKey.G)) {
-      this.angle = 0;
+    if (inputHandler.isKeyPressed(InputKey.A) && this.direction === "left") {
+      this.moveLeft();
+      return;
     }
 
-    if (inputHandler.isKeyPressed(InputKey.W)) {
-      this.posY += this.speed * Math.sin(((this.angle - 90) * Math.PI) / 180);
-      this.posX += this.speed * Math.cos(((this.angle - 90) * Math.PI) / 180);
-
-      if (yCollision && (this.angle <= -90 || this.angle >= 90)) {
-        this.speed = this.initialSpeed;
-        return;
-      }
+    if (inputHandler.isKeyPressed(InputKey.D) && this.direction === "right") {
+      this.moveRight();
+      return;
     }
 
-    if (inputHandler.isKeyPressed(InputKey.S)) {
-      this.posY -= this.speed * Math.sin(((this.angle - 90) * Math.PI) / 180);
-      this.posX -= this.speed * Math.cos(((this.angle - 90) * Math.PI) / 180);
+    this.direction = "none";
+  }
 
-      if (
-        xCollision &&
-        (this.posX >= this.width * 0.5 || this.posX >= this.width * -0.5)
-      ) {
-        this.speed = this.initialSpeed;
-        return;
-      }
+  private moveForward(): void {
+    this.posY -= this.speed;
+  }
 
-      if (
-        yCollision &&
-        (this.posY >= this.height * 0.5 || this.posY >= this.height * -0.5)
-      ) {
-        this.speed = this.initialSpeed;
-        return;
-      }
-    }
+  private moveBakward(): void {
+    this.posY += this.speed;
+  }
+
+  private moveRight(): void {
+    this.posX += this.speed;
+  }
+
+  private moveLeft(): void {
+    this.posX -= this.speed;
   }
 
   private handleWorldCollision(): void {
-    const { xCollision, yCollision } = this.collisionData;
+    const { topCollision, rightCollision, bottomCollision, leftCollision } =
+      this.collisionData;
+    const { inputHandler } = this.gameData;
 
-    if (xCollision || yCollision) {
+    if (
+      (topCollision && inputHandler.isKeyPressed(InputKey.W)) ||
+      (rightCollision && inputHandler.isKeyPressed(InputKey.D)) ||
+      (bottomCollision && inputHandler.isKeyPressed(InputKey.S)) ||
+      (leftCollision && inputHandler.isKeyPressed(InputKey.A))
+    ) {
       this.speed = 0;
+    } else {
+      this.speed = this.initialSpeed;
     }
   }
 
+  private getCollisionData(gameGeometry: {
+    width: number;
+    height: number;
+  }): CollisionData {
+    return {
+      xCollision:
+        this.posX - this.speed < this.width * 0.5 ||
+        this.posX + this.speed + this.width * 0.5 > gameGeometry.width,
+      yCollision:
+        this.posY - this.speed < this.height * 0.5 ||
+        this.posY + this.speed + this.height * 0.5 > gameGeometry.height,
+
+      topCollision: this.posY - this.speed < this.height * 0.5,
+      rightCollision:
+        this.posX + this.speed + this.width * 0.5 > gameGeometry.width,
+      bottomCollision:
+        this.posY + this.speed + this.height * 0.5 > gameGeometry.height,
+      leftCollision: this.posX - this.speed < this.width * 0.5,
+    };
+  }
+
   public draw(context: CanvasRenderingContext2D): void {
-    // Stack Overflow link about rotation
-    // https://stackoverflow.com/questions/17125632/html5-canvas-rotate-object-without-moving-coordinates
-
     context.save();
+    this.drawContent(context);
+    context.restore();
+  }
 
-    context.fillStyle = colors.nord.green;
-
-    // Note that the context is now in its rotated state.
-    // That means drawing position [0,0] is visually at [ posX + this.width * 0.5, posY + this.height * 0.5 ].
-    context.translate(this.posX, this.posY);
-
-    // The (this.angle * Math.PI) / 180 is trasformation of angle value in degrees to radians
-    context.rotate((this.angle * Math.PI) / 180);
-
-    // you must draw the rotated rect at [ this.width * -0.5, this.height * -0.5 ] (not at the original unrotated x/y).
-    context.fillRect(
-      this.width * -0.5,
-      this.height * -0.5,
-      this.width,
-      this.height
-    );
+  public drawContent(context: CanvasRenderingContext2D): void {
+    this.drawPlayer(context);
 
     if (this.isShowHelpers) {
       this.drawHelpers({
         context,
+        isShowCorners: false,
       });
     }
+  }
 
-    context.restore();
+  public drawPlayer(context: CanvasRenderingContext2D): void {
+    context.fillStyle = colors.nord.green;
+    context.fillRect(
+      this.posX - this.width * 0.5,
+      this.posY - this.height * 0.5,
+      this.width,
+      this.height
+    );
   }
 
   private drawHelpers({
@@ -176,79 +199,103 @@ export class Player extends GameObject {
     context: CanvasRenderingContext2D;
     isShowCorners?: boolean;
   }): void {
-    const width = 10;
-    const height = 10;
-
     context.fillStyle = colors.nord.red;
 
-    // center
-    context.fillRect(-5, -5, 10, 10);
+    this.drawCenterHelper(context);
+    this.drawDirectionHelper(context);
+    this.drawTextHelpers(context);
 
     if (isShowCorners) {
-      //top right
-      context.fillRect(
-        this.width * 0.5 - width,
-        this.height * -0.5,
-        width,
-        height
-      );
-
-      //bottom right
-      context.fillRect(
-        this.width * 0.5 - width,
-        this.height * 0.5 - height,
-        width,
-        height
-      );
-
-      //bottom left
-      context.fillRect(
-        this.width * -0.5,
-        this.height * 0.5 - height,
-        width,
-        height
-      );
-
-      //top left
-      context.fillRect(this.width * -0.5, this.height * -0.5, width, height);
+      this.drawCornersHelpers(context);
     }
+  }
 
-    // direction line
-    context.strokeStyle = colors.nord.red;
-    context.beginPath();
-    context.moveTo(0, this.height * -0.5);
-    context.lineTo(0, 0);
-    context.stroke();
-
-    // position text
+  private drawTextHelpers(context: CanvasRenderingContext2D): void {
     context.font = "16px 'Yanone Kaffeesatz'";
     context.fillText(
       `x: ${this.posX.toFixed(0)}`,
-      this.width * -0.5,
-      this.height * -0.5 - 30
+      this.posX - this.width * 0.5,
+      this.posY - this.height * 0.5 - 50
     );
     context.fillText(
       `y: ${this.posY.toFixed(0)}`,
-      this.width * -0.5,
-      this.height * -0.5 - 10
-    );
-
-    context.fillText(
-      `Angle: ${this.angle}`,
-      this.width * -0.5,
-      this.height * 0.5 - 5
+      this.posX - this.width * 0.5,
+      this.posY - this.height * 0.5 - 30
     );
 
     context.fillText(
       `Speed: ${this.speed}`,
-      this.width * -0.5,
-      this.height * 0.5 + 17
+      this.posX - this.width * 0.5,
+      this.posY - this.height * 0.5 - 10
+    );
+  }
+
+  private drawCenterHelper(context: CanvasRenderingContext2D): void {
+    const width = 10;
+    const height = 10;
+
+    context.fillRect(this.posX - width * 0.5, this.posY - height * 0.5, 10, 10);
+  }
+
+  private drawCornersHelpers(context: CanvasRenderingContext2D): void {
+    const width = 10;
+    const height = 10;
+
+    //top left
+    context.fillRect(
+      this.posX - this.width * 0.5,
+      this.posY - this.height * 0.5,
+      width,
+      height
     );
 
-    context.fillText(
-      `Rotation spd: ${this.rotationSpeed}`,
-      this.width * -0.5,
-      this.height * 0.5 + 34
+    //top right
+    context.fillRect(
+      this.posX + this.width * 0.5 - width,
+      this.posY - this.height * 0.5,
+      width,
+      height
     );
+
+    //bottom right
+    context.fillRect(
+      this.posX + this.width * 0.5 - width,
+      this.posY + this.height * 0.5 - height,
+      width,
+      height
+    );
+
+    //bottom left
+    context.fillRect(
+      this.posX - this.width * 0.5,
+      this.posY + this.height * 0.5 - height,
+      width,
+      height
+    );
+  }
+
+  private drawDirectionHelper(context: CanvasRenderingContext2D): void {
+    context.strokeStyle = colors.nord.red;
+    context.beginPath();
+
+    context.moveTo(this.posX, this.posY);
+
+    if (this.direction === "top") {
+      context.lineTo(this.posX, this.posY - this.width * 0.5);
+    }
+
+    if (this.direction === "right") {
+      context.lineTo(this.posX + this.width * 0.5, this.posY);
+    }
+
+    if (this.direction === "bottom") {
+      context.lineTo(this.posX, this.posY + this.width * 0.5);
+    }
+
+    if (this.direction === "left") {
+      context.lineTo(this.posX - this.width * 0.5, this.posY);
+    }
+
+    context.stroke();
   }
 }
