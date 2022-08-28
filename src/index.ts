7c6768b896dbdf49909692f5e153696f91928a42
Alexander Hridin <alexander.m.gridin@gmail.com>
2022-08-28 14:29:30 +0300

    removed useToggler

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,14 @@
 import { Canvas } from "./classes/canvas.class";
 import { Game } from "./classes/game.class";
-import { FpsCounter } from "./classes/fps-counter.class";
-import { useToggler } from "./utils/use-toggler.util";
 
 const main = () => {
   const canvas = new Canvas({
     selector: "#canvas",
+    width: 860,
+    height: 480,
   });
 
-  const game = new Game(canvas).addObjects([new FpsCounter()]);
-
-  useToggler({
-    selector: "button",
-    activeText: "Stop",
-    disabledText: "Start",
-    useWhenActive: game.start.bind(game),
-    useWhedDisabled: game.stop.bind(game),
-  });
+  new Game(canvas);
 };
 
 window.addEventListener("load", main);
